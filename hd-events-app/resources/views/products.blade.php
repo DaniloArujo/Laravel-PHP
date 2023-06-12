e36708ae1253359a47c2292179ebeaebbb1a0f5e
Danilo <daniloaraujomota@gmail.com>
2023-06-12 08:50:23 -0300

    .

--- hd-events-app/resources/views/products.blade.php
+++ hd-events-app/resources/views/products.blade.php
@@ -1,1 +1,8 @@
+@extends('layouts.main')
+
+@section('title', 'produtos')
+
+@section('content')
+
 <h1>Esta é a minha tela de produtos</h1>
+@endsection
--- hd-events-app/resources/views/welcome.blade.php
+++ hd-events-app/resources/views/welcome.blade.php
@@ -1,47 +1,37 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.main')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
+@section('title','HDC events')
 
 
-    <link rel="stylesheet" href="/css/styles.css">
-    <script src="/js/scripts.js"></script>
+@section('content')
+<h1>Algum titulo</h1>
 
-</head>
-
-<body class="antialiased">
-    <h1>Algum titulo</h1>
-
-    <img src="/img/Screenshot_1.png" alt="Banner">
+<img src="/img/Screenshot_1.png" alt="Banner">
 
 
-    @if( $idade > 18)
-    <p>{{$nome}} é maior de idade</p>
-    @elseif( $idade < 18 and $idade> 12 )
-        <p>{{$nome}} é adolescente</p>
-        @else
-        <p>{{$nome}} é criança</p>
-        @endif
+@if( $idade > 18)
+<p>{{$nome}} é maior de idade</p>
+@elseif( $idade < 18 and $idade> 12 )
+    <p>{{$nome}} é adolescente</p>
+    @else
+    <p>{{$nome}} é criança</p>
+    @endif
 
-    <p> {{$nome}} tem {{$idade}} anos</p>
-    @for($i = 0; $i < count($arr); $i++)
-        <p>{{$arr[$i]}}0 - {{$arr[$i]}}</p>
-        @if($i == 2)
-        <p>O i é 2</p>
-        @endif
-    @endfor
+<p> {{$nome}} tem {{$idade}} anos</p>
+@for($i = 0; $i < count($arr); $i++)
+    <p>{{$arr[$i]}}0 - {{$arr[$i]}}</p>
+    @if($i == 2)
+    <p>O i é 2</p>
+    @endif
+@endfor
 
-    @foreach($nomes as $nome)
-        <p>{{$nome}}</p>
-    @endforeach
-    @php
-        $name = "João";
-        echo $name;
-    @endphp
-    <!-- comentário html-->
-    {{--comentário blade--}}
-</html>
+@foreach($nomes as $nome)
+    <p>{{$nome}}</p>
+@endforeach
+@php
+    $name = "João";
+    echo $name;
+@endphp
+<!-- comentário html-->
+{{--comentário blade--}}
+@endsection
